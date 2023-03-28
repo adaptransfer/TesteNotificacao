c5748da66b50ab01a4ccf655ec6eb44c99e6597a
Igor12x <igorbtenorioidi@gmail.com>
2023-03-28 16:15:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/testenotificacao/NotificacaoActivity.java
@@ -0,0 +1,14 @@
+package com.example.testenotificacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacao);
+    }
+}
